9919ae9eba692d83ec94cafca9e5090f129e2d0d
“ivanleshev” <leshevi@inbox.ru>
2021-07-14 20:59:04 +0400

    переопределил реализацию интерфейса для подкласса

--- Homework11_2/Program.cs
+++ Homework11_2/Program.cs
@@ -18,11 +18,25 @@ namespace Homework11_2
         }
 
         void I1.M()
-        {            
+        {
+            Console.WriteLine("I1.M()");
         }
 
         void I2.M()
         {
+            Console.WriteLine("I2.M()");
+        }
+    }
+
+    class B : A, I1, I2
+    {
+        void I1.M()
+        {
+            Console.WriteLine("B.I1.M()");
+        }
+        void I2.M()
+        {
+            Console.WriteLine("B.I2.M()");
         }
     }
 }
