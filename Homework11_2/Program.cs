c47e5ade9a0cd0c74022aa3107e2079e48a28b66
“ivanleshev” <leshevi@inbox.ru>
2021-07-14 20:48:20 +0400

    Начальный возврат модуля Homework11_2

---
+++ Homework11_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework11_2
+{
+    interface I1
+    {
+        void M();
+    }
+    interface I2
+    {
+        void M();
+    }
+    class A : I1, I2
+    {
+        public void M()
+        {
+            Console.WriteLine("A.M()");
+        }
+
+        void I1.M()
+        {            
+        }
+
+        void I2.M()
+        {
+        }
+    }
+}
